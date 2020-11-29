fa0dcff298a79500e086c273f4e2ad219056c13b
arunkumarkalai13 <75129159+arunkumarkalai13@users.noreply.github.com>
2020-11-29 21:23:26 +0530

    Update prblm1.js

--- prblm1.js
+++ prblm1.js
@@ -1,16 +1,20 @@
-const num = 12344
-const num1 = 22;
 let repeat = (num) => {
 const res = num.toString();
 count = 0;
-    for(i = 0;i<res.length-1;i++){
-        if(res[i] === res[i+1]){
-                count++;
+let len = res.length - 1;
+        if(res[len-1] === res[len])
+        { 
+            return true; 
         }
-    }
-    if(count > 0){ return true; }
-    else{ return false; }
+        else { 
+            return false; 
+        }
 }
+// test cases
+const num = 1234423
+const num1 = 22;
+const num2 = 12344
 
 console.log(repeat(num));
 console.log(repeat(num1));
+console.log(repeat(num2));
